008e8fca793b2a86a0fa061c442ad9570d1cbd84
heloaaires <heloa.andrade@gmail.com>
2022-07-30 04:47:07 -0300

    :bug: fix bug .env

--- src/database/mongooseConnect.js
+++ src/database/mongooseConnect.js
@@ -1,11 +1,11 @@
 const DATABASE_URI = process.env.DATABASE_URI
 
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 
 const connect = async () => {
     try {
-        mongoose.connect(DATABASE_URI, {
+        await mongoose.connect(DATABASE_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
